0197625037ef3b49338cd9570a8b7f48ec1a9870
lelele <changle0703@outlook.com>
2022-01-28 01:04:07 +0000

    This is updated data augmentation.

--- AudioDataset.py
+++ AudioDataset.py
@@ -6,14 +6,36 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import torchaudio
-
+import time
 class AudioDataset(Dataset):
     
-    def __init__(self, label_df, audio_dir, transforms, target_sample_rate, length, device, is_train_or_valid=True):
+    def __init__(self, label_df, audio_dir, transforms, target_sample_rate, length, device, is_train_or_valid=True, do_augmentation = False):
         super().__init__()
         self.label_df = label_df
         self.audio_dir = audio_dir
         self.device = device
+        self.do_augmentation = do_augmentation
+        pitch_shiftor = torchaudio.transforms.PitchShift(
+            target_sample_rate, torch.randint(low=0,high=6,size=(1,))
+        )
+
+        spectrogram = torchaudio.transforms.Spectrogram(
+            n_fft=1024,
+            hop_length=512
+        )
+
+        freq_masking = torchaudio.transforms.FrequencyMasking(freq_mask_param=30)
+
+        melscale = torchaudio.transforms.MelScale(
+            sample_rate=target_sample_rate,
+            n_mels=64,
+            n_stft=513
+        )
+        if self.do_augmentation:
+            transforms = [spectrogram, freq_masking, melscale]
+        else:
+            transforms = [spectrogram, melscale]
+        self.transforms = transforms
         if isinstance(transforms, list):
             self.transforms = [transform.to(self.device) for transform in transforms]
         else:
@@ -28,20 +50,37 @@ class AudioDataset(Dataset):
     
     def __getitem__(self, index):
         # no indexOutOfRange exception
+        #A = time.time()
         audio_sample_path = self._get_sample_path(index)
         signal, sample_rate = torchaudio.load(audio_sample_path)
+        
+        if self.do_augmentation:
+            signal = signal + (torch.randn(signal.shape)-0.5)*0.002
         if signal.dim() == 1:
             signal = signal[None,:]
+
         signal = signal.to(self.device)
+        
         signal = self._resample(signal, sample_rate)
         signal = self._mix(signal)
         signal = self._truncate(signal)
         signal = self._pad(signal)
+
+        #B = time.time()
+
         if isinstance(self.transforms, list):
             for transform in self.transforms:
                 signal = transform(signal)
         else:
             signal = self.transforms(signal)
+        
+        #C = time.time()
+        
+        #signal = signal.to(self.device)
+
+        #D = time.time()
+        #print(B-A,C-B,D-C)
+        #print(signal.shape)
         
         if self.is_train_or_valid:
             audio_label = self._get_sample_label(index)
